8e882207fc9e8f531c6c43662bf85bbe24397aab
Joshua Foster <joshua.david.foster@gmail.com>
2016-01-26 23:33:55 +0000

    Class created for the boids data structure
    The boids structure were refactored into a class (Boids) with
    generate_boids, generate_from_file, shift_boid and update_boids as
    methods. The unit tests were updated to work with the new Boids class.

--- boids.py
+++ boids.py
@@ -3,79 +3,94 @@ from matplotlib import animation
 import random
 
 
-def generate_boids(count):
-        boids_x = [random.uniform(-450, 50.0) for x in range(count)]
-        boids_y = [random.uniform(300.0, 600.0) for x in range(count)]
-        boid_x_velocities = [random.uniform(0, 10.0) for x in range(count)]
-        boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(count)]
-        boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-        return boids
+class Boids(object):
+        def __init__(self, boid_count, flock_attraction, avoid_radius,
+                     flock_radius, velocity_matching):
+                self.count = boid_count
+                self.flock_attraction = flock_attraction / self.count
+                self.avoid_radius = avoid_radius
+                self.flock_radius = flock_radius
+                self.velocity_matching = velocity_matching / self.count
 
+        def generate_boids(self):
+                self.xs = [random.uniform(-450, 50.0) for x in
+                           range(self.count)]
+                self.ys = [random.uniform(300.0, 600.0) for x in
+                           range(self.count)]
+                self.xvs = [random.uniform(0, 10.0) for x in
+                            range(self.count)]
+                self.yvs = [random.uniform(-20.0, 20.0) for x in
+                            range(self.count)]
 
-def shift_boid(own_x, own_y, own_xv, own_yv,
-               other_x, other_y, other_xv, other_yv):
-        x_delta = 0
-        y_delta = 0
-        x_difference = other_x - own_x
-        y_difference = other_y - own_y
-        sum_of_squares = x_difference**2 + y_difference**2
+        def generate_from_file(self, file_data):
+                self.xs, self.ys, self.xvs, self.yvs = file_data
 
-        # Fly towards the middle
-        x_delta += x_difference * flock_attraction
-        y_delta += y_difference * flock_attraction
-
-        # Fly away from nearby boids
-        if sum_of_squares < avoid_radius:
-                x_delta -= x_difference
-                y_delta -= y_difference
-
-        # Try to match speed with nearby boids
-        if sum_of_squares < flock_radius:
-                x_delta += (other_xv - own_xv) * velocity_matching
-                y_delta += (other_yv - own_yv) * velocity_matching
-
-        return [x_delta, y_delta]
-
-
-def update_boids(boids):
-        xs, ys, xvs, yvs = boids
-        boid_len = len(xs)
-
-        for i in range(boid_len):
+        def shift_boid(self, own_x, own_y, own_xv, own_yv,
+                       other_x, other_y, other_xv, other_yv):
                 x_delta = 0
                 y_delta = 0
+                x_difference = other_x - own_x
+                y_difference = other_y - own_y
+                sum_of_squares = x_difference**2 + y_difference**2
 
-                for j in range(boid_len):
-                        shift_values = shift_boid(xs[i], ys[i], xvs[i], yvs[i],
-                                                  xs[j], ys[j], xvs[j], yvs[j])
-                        x_delta += shift_values[0]
-                        y_delta += shift_values[1]
+                # Fly towards the middle
+                x_delta += x_difference * self.flock_attraction
+                y_delta += y_difference * self.flock_attraction
 
-                # Adjust velocities from interaction
-                xvs[i] += x_delta
-                yvs[i] += y_delta
+                # Fly away from nearby boids
+                if sum_of_squares < self.avoid_radius:
+                        x_delta -= x_difference
+                        y_delta -= y_difference
 
-                # Move according to velocities
-                xs[i] += xvs[i]
-                ys[i] += yvs[i]
+                # Try to match speed with nearby boids
+                if sum_of_squares < self.flock_radius:
+                        x_delta += (other_xv - own_xv) * self.velocity_matching
+                        y_delta += (other_yv - own_yv) * self.velocity_matching
+
+                return [x_delta, y_delta]
+
+        def update_boids(self):
+                for i in range(self.count):
+                        x_delta = 0
+                        y_delta = 0
+
+                        for j in range(self.count):
+                                shift_values = self.shift_boid(
+                                        self.xs[i], self.ys[i],
+                                        self.xvs[i], self.yvs[i],
+                                        self.xs[j], self.ys[j],
+                                        self.xvs[j], self.yvs[j])
+                                x_delta += shift_values[0]
+
+                                y_delta += shift_values[1]
+
+                        # Adjust velocities from interaction
+                        self.xvs[i] += x_delta
+                        self.yvs[i] += y_delta
+
+                        # Move according to velocities
+                        self.xs[i] += self.xvs[i]
+                        self.ys[i] += self.yvs[i]
 
 
 boid_count = 50
-flock_attraction = 0.01 / boid_count
+flock_attraction = 0.01
 avoid_radius = 100
 flock_radius = 10000
-velocity_matching = 0.125 / boid_count
+velocity_matching = 0.125
 boundry_limits = [-500, 1500, -500, 1500]
-boids = generate_boids(boid_count)
+boids = Boids(boid_count, flock_attraction, avoid_radius, flock_radius,
+              velocity_matching)
+boids.generate_boids()
 figure = plt.figure()
 axes = plt.axes(xlim=(boundry_limits[0], boundry_limits[1]),
                 ylim=(boundry_limits[2], boundry_limits[3]))
-scatter = axes.scatter(boids[0], boids[1])
+scatter = axes.scatter(boids.xs, boids.ys)
 
 
 def animate(frame):
-        update_boids(boids)
-        scatter.set_offsets(zip(boids[0], boids[1]))
+        boids.update_boids()
+        scatter.set_offsets(zip(boids.xs, boids.ys))
 
 
 anim = animation.FuncAnimation(figure, animate,
--- test_boids.py
+++ test_boids.py
@@ -1,45 +1,51 @@
-from boids import update_boids, generate_boids, shift_boid
+import boids as bdz
 from nose.tools import assert_almost_equal, assert_equal, assert_less, assert_greater
 import os
 import yaml
 
 
 def test_bad_boids_regression():
+    boids = bdz.Boids(50, 0.01, 100, 10000, 0.125)
     regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
-    boid_data = regression_data["before"]
-    update_boids(boid_data)
-    for after, before in zip(regression_data["after"], boid_data):
+    boids.generate_from_file(regression_data["before"])
+    boids.update_boids()
+    actual_values = (boids.xs, boids.ys, boids.xvs, boids.yvs)
+    for after, before in zip(regression_data["after"], actual_values):
         for after_value, before_value in zip(after, before):
             assert_almost_equal(after_value, before_value, delta=0.01)
 
 
 def test_generate_boids():
-    xs, ys, xvs, yvs = generate_boids(50)
-    assert_equal(len(xs), 50)
-    for x_value in xs:
+    boids = bdz.Boids(50, 0.01, 100, 10000, 0.125)
+    boids.generate_boids()
+    assert_equal(len(boids.xs), 50)
+    for x_value in boids.xs:
         assert_less(x_value, 50)
         assert_greater(x_value, -450)
-    for y_value in ys:
+    for y_value in boids.ys:
         assert_less(y_value, 600)
         assert_greater(y_value, 300)
-    for xv_value in xvs:
+    for xv_value in boids.xvs:
         assert_less(xv_value, 10)
         assert_greater(xv_value, 0)
-    for yv_value in yvs:
+    for yv_value in boids.yvs:
         assert_less(yv_value, 20)
         assert_greater(yv_value, -20)
 
 
 def test_shift_boid_flock_centering():
     # Uses default global values for flock centring
-    assert_equal(shift_boid(0, 0, 0, 0, 8, 8, 50, 50), [0.1266, 0.1266])
+    boids = bdz.Boids(50, 0.01, 100, 10000, 0.125)
+    assert_equal(boids.shift_boid(0, 0, 0, 0, 8, 8, 50, 50), [0.1266, 0.1266])
 
 
 def test_shift_boid_collision_avoidance():
     # Uses default global values for collision avoidance
-    assert_equal(shift_boid(0, 0, 0, 0, 4, 4, 50, 50), [-3.8742, -3.8742])
+    boids = bdz.Boids(50, 0.01, 100, 10000, 0.125)
+    assert_equal(boids.shift_boid(0, 0, 0, 0, 4, 4, 50, 50), [-3.8742, -3.8742])
 
 
 def test_shift_boid_velocity_matching():
     # Uses default global values for velocity matching
-    assert_equal(shift_boid(0, 0, 0, 0, 80, 80, 50, 50), [0.016, 0.016])
+    boids = bdz.Boids(50, 0.01, 100, 10000, 0.125)
+    assert_equal(boids.shift_boid(0, 0, 0, 0, 80, 80, 50, 50), [0.016, 0.016])
