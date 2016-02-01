ec7b0ea2bed1492aaaf0047616412b5fd252f3a4
Joshua Foster <joshua.david.foster@gmail.com>
2016-02-01 13:00:44 +0000

    Created ViewBoids and ControllerBoids classes
    Created additional classes for the Model-View-Controller pattern. These
    classes separate the responsibility for the boids model, plotting and
    animation.

--- view_boids.py
+++ view_boids.py
@@ -5,31 +5,51 @@ import os
 import yaml
 
 
-with open(os.path.join(os.path.dirname(__file__), 'boid_config.yml')) \
-     as config_file:
-    config = yaml.load(config_file)
-    boundary_limits = config.pop('boundary_limits')
-    builder = BoidsBuilder()
-    builder.set_flock_parameters(**config)
-    builder.generate_boids()
-    boids = builder.finish()
+class ViewBoids(object):
+    def __init__(self, boids_model, boundary_limits):
+        self.boids = boids_model
+        self.boundary_limits = boundary_limits
+        self.figure = plt.figure()
+        axes = plt.axes(xlim=(self.boundary_limits[0],
+                              self.boundary_limits[1]),
+                        ylim=(self.boundary_limits[2],
+                              self.boundary_limits[3]))
+        self.scatter = axes.scatter([bd.location[0] for bd
+                                     in self.boids.flock],
+                                    [bd.location[1] for bd
+                                     in self.boids.flock])
+
+    def update_plt(self):
+        self.scatter.set_offsets([bd.location for bd in self.boids.flock])
 
 
-figure = plt.figure()
-axes = plt.axes(xlim=(boundary_limits[0], boundary_limits[1]),
-                ylim=(boundary_limits[2], boundary_limits[3]))
-scatter = axes.scatter([bd.location[0] for bd in boids.flock],
-                       [bd.location[1] for bd in boids.flock])
+class ControllerBoids(object):
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__),
+                               'boid_config.yml')) as config_file:
+            config = yaml.load(config_file)
+            boundary_limits = config.pop('boundary_limits')
+            builder = BoidsBuilder()
+            builder.set_flock_parameters(**config)
+            builder.generate_boids()
+            self.boids = builder.finish()
+            self.view = ViewBoids(self.boids, boundary_limits)
+        
+        def animate_boid(frame_number):
+            self.boids.update_boids()
+            self.view.update_plt()
 
+        self.animator = animate_boid
 
-def animate(frame):
-        boids.update_boids()
-        scatter.set_offsets([bd.location for bd in boids.flock])
+    def run_animation(self):
+        anim = animation.FuncAnimation(self.view.figure, self.animator,
+                                       frames=50, interval=50)
+        return anim
 
-
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+    
+contrl = ControllerBoids()
+anim = contrl.run_animation()
 
 
 if __name__ == "__main__":
-        plt.show()
+    plt.show()
