b04b49e0b9895332ef421c87120d690cd236023b
Joshua Foster <joshua.david.foster@gmail.com>
2016-02-16 18:09:32 +0000

    Added a unit test to compare generated plot
    The added tests to compare the first two generated plots from the
    fixture file are as expected.

--- boids/tests/test_viewer.py
+++ boids/tests/test_viewer.py
@@ -1,6 +1,11 @@
 from ..viewer import ViewBoids
+from ..builder import BuildBoids
 from mock import patch, MagicMock
+from matplotlib import image as img
+from StringIO import StringIO
 import numpy as np
+import os
+import yaml
 
 
 def test_ViewBoids():
@@ -15,3 +20,30 @@ def test_ViewBoids():
         view.update_plt()
         called_with = view.scatter.set_offsets.call_args[0][0]
         np.testing.assert_array_equal(called_with, test_array)
+
+
+def compare_view_generated_plot(view_obj, test_image):
+    buffer = StringIO()
+    view_obj.figure.savefig(buffer)
+    actual_array = img.imread(StringIO(buffer.getvalue()))
+    test_array = img.imread(test_image)
+    np.testing.assert_almost_equal(actual_array, test_array)
+
+
+def test_plot_comparison():
+    with open(os.path.join(os.path.dirname(__file__), 'fixtures',
+                           'fixture.yml'), 'r') as fixture_file, \
+    open(os.path.join(os.path.dirname(__file__), 'fixtures',
+                      'first.png'), 'r') as first_image, \
+    open(os.path.join(os.path.dirname(__file__), 'fixtures',
+                      'second.png'), 'r') as second_image:
+        builder = BuildBoids()
+        builder.set_flock_parameters(50, 0.01, 100, 10000, 0.125)
+        regression_data = yaml.load(fixture_file)
+        builder.generate_from_file(regression_data["before"])
+        boids = builder.finish()
+        view = ViewBoids(boids, [-500, 1500], [-500, 1500])
+        for test_image in [first_image, second_image]:
+            compare_view_generated_plot(view, test_image)
+            boids.update_boids()
+            view.update_plt()
