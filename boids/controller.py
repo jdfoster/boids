56a4b0bcf971463f9fe5ce7a8df30ddf50f11286
Joshua Foster <joshua.david.foster@gmail.com>
2016-02-21 19:05:11 +0000

    Added exceptions to ControlBoids class
    Created exceptions to raise type errors for incorrect values for the
    animation settings given to the ControlBoids class. Additional tests
    are needed to check these exceptions are correctly given.

--- boids/controller.py
+++ boids/controller.py
@@ -5,7 +5,6 @@ from matplotlib import animation
 
 class ControlBoids(object):
     def __init__(self, settings):
-        self.anim_settings = settings.pop('animation_settings')
         builder = BuildBoids()
         builder.set_location_ranges(**settings.pop('location_range'))
         builder.set_velocity_ranges(**settings.pop('velocity_range'))
@@ -13,6 +12,7 @@ class ControlBoids(object):
         builder.generate_boids()
         self.boids = builder.finish()
         self.view = ViewBoids(self.boids, **settings.pop('boundary_limits'))
+        self._set_animation_settings(**settings.pop('animation_settings'))
 
         def animate_boid(frame_number):
             self.boids.update_boids()
@@ -20,9 +20,18 @@ class ControlBoids(object):
 
         self.animator = animate_boid
 
+    def _set_animation_settings(self, frames, interval):
+        anim_settings_integer = [isinstance(frames, int),
+                                 isinstance(interval, int)]
+        if not all(anim_settings_integer):
+            raise TypeError('The animation settings frames and interval ' +
+                            'need to be integers')
+        self.anim_frames = frames
+        self.anim_interval = interval
+
     def run_animation(self):
         anim = animation.FuncAnimation(self.view.figure, self.animator,
-                                       frames=self.anim_settings['frames'],
-                                       interval=self.anim_settings['interval'],
+                                       frames=self.anim_frames,
+                                       interval=self.anim_interval,
                                        repeat=0)
         return anim
