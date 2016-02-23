1248b4f28b69741b69ad943482feb92fe4e0cf47
Joshua Foster <joshua.david.foster@gmail.com>
2016-02-23 10:16:57 +0000

    Replaced repeating exception code with a method
    The code used to check for an exception was repeated in the two class
    method, this was refactored to using a single function. This could be
    possibly reduced further using functional programming.

--- boids/boid_exceptions.py
+++ boids/boid_exceptions.py
@@ -3,39 +3,40 @@ class BoidExceptions(object):
                 bool_list = [isinstance(item, dat_type) for item in container]
                 return all(bool_list)
 
+    def _check_exception(self, test_list, dat_type, er_type):
+        try:
+            assert self._list_type_check(test_list, dat_type)
+
+        except AssertionError:
+            raise er_type
+
     @classmethod
     def check_xy_limits(self, funct):
         def _wrapped_funct(self, *args, **kwargs):
-
-            def _check(self, test_arg):
-                try:
-                    assert self._list_type_check(test_arg, float)
-
-                except AssertionError:
-                    raise TypeError('X-Y limits should be a list with two ' +
-                                    'floating point values')
+            er_type = TypeError('X-Y limits should be a list with two ' +
+                                'floating point values')
 
             if len(args) == 2:
                 for arg in args:
-                    _check(self, arg)
+                    self._check_exception(arg, float, er_type)
 
             if len(kwargs) > 0:
-                _check(self, kwargs['x_limits'])
-                _check(self, kwargs['y_limits'])
+                self._check_exception(kwargs['x_limits'], float, er_type)
+                self._check_exception(kwargs['y_limits'], float, er_type)
 
             funct(self, *args, **kwargs)
+
         return _wrapped_funct
 
     @classmethod
     def check_flock_params(self, funct):
         def _wrapped_funct(self, *args, **kwargs):
-
-            def _check(self, test_arg, dat_type):
-                try:
-                    assert self._list_type_check(test_arg, dat_type)
-
-                except AssertionError:
-                    raise TypeError
+            int_er_type = TypeError('Flock parameters boid_count, ' +
+                                    'avoid_radius and flock_radius ' +
+                                    'should be integer values')
+            float_er_type = TypeError('Flock parameters flock_attraction ' +
+                                      'and velocity_matching should be ' +
+                                      'floating point values')
 
             if len(args) == 5:
                 list_int = [args[0], args[2], args[3]]
@@ -48,7 +49,8 @@ class BoidExceptions(object):
                 list_float = [kwargs['flock_attraction'],
                               kwargs['velocity_matching']]
 
-            _check(self, list_int, int)
-            _check(self, list_float, float)
+            self._check_exception(list_int, int, int_er_type)
+            self._check_exception(list_float, float, float_er_type)
             funct(self, *args, **kwargs)
+
         return _wrapped_funct
